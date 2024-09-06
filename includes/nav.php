7f2236319e60e93e24717e47b41e538e8a655a21
Chandan <chandanbhanopa@gmail.com>
2024-09-06 19:03:08 +0530

    update page contents

---
+++ includes/nav.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">BOG</a>
+        <form action="#" class="searchform order-sm-start order-lg-last">
+            <div class="form-group d-flex">
+                <input type="text" class="form-control pl-3" placeholder="Search" />
+                <button type="submit" placeholder="" class="form-control search">
+                    <span class="fa fa-search"></span>
+                </button>
+            </div>
+        </form>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+            aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="about.php" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="services.php" class="nav-link">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a href="cases.php" class="nav-link">Case Study</a>
+                </li>
+                <li class="nav-item">
+                    <a href="blog.php" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.php" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
