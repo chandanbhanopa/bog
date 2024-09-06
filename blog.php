018c22a691212226541a3213cfa411c52c322059
Chandan <chandanbhanopa@gmail.com>
2024-09-06 19:14:26 +0530

    fix css issue

--- blog.php
+++ blog.php
@@ -20,32 +20,6 @@
 
   <div class="wrap">
     <?php include "includes/header.php";?>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="bg-wrap">
-          <div class="row">
-            <div class="col-md-6 d-flex align-items-center">
-              <p class="mb-0 phone pl-md-2">
-                <a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span>(+91)97526-29888</a>
-                <a href="#"><span class="fa fa-paper-plane mr-1"></span>admin@bogtrading.com</a>
-              </p>
-            </div>
-            <div class="col-md-6 d-flex justify-content-md-end">
-              <div class="social-media">
-                <p class="mb-0 d-flex">
-                  <a href="https://www.facebook.com/profile.php?id=61565422674185" target="_blank"
-                    class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i
-                        class="sr-only">Facebook</i></span></a>
-                  <a href="https://x.com/BogTrading" target="_blank"
-                    class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i
-                        class="sr-only">Twitter</i></span></a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
   </div>
   <?php include "includes/nav.php";?>
